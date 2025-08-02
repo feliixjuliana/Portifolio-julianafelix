b5512dea46de9a62d0be548ce764a0fa8f050ab5
juliana <user.taniajuju91@gmail.com>
2025-08-02 02:20:53 -0300

    Feat: Pag projeto

---
+++ script.js
@@ -0,0 +1,23 @@
+const track = document.querySelector('.carousel-track');
+const items = document.querySelectorAll('.carousel-item');
+const prevButton = document.querySelector('.carousel-button.prev');
+const nextButton = document.querySelector('.carousel-button.next');
+
+let currentIndex = 0;
+
+function updateCarousel() {
+  const width = items[0].clientWidth;
+  track.style.transform = `translateX(-${currentIndex * width}px)`;
+}
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % items.length;
+  updateCarousel();
+});
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + items.length) % items.length;
+  updateCarousel();
+});
+
+window.addEventListener('resize', updateCarousel);
